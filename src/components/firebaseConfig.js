c54aa88ce4036839c635995a12c6cc7d3f1f5989
DDOSNonpavit <non1996@hotmail.com>
2018-11-17 17:58:12 +0700

    v.1

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCo_fHsl1-l95ETToj0t34bR1KrNFQ2ays",
+  authDomain: "ywc16-25719.firebaseapp.com",
+  databaseURL: "https://ywc16-25719.firebaseio.com",
+  projectId: "ywc16-25719",
+  storageBucket: "ywc16-25719.appspot.com",
+  messagingSenderId: "178778416390"
+};
